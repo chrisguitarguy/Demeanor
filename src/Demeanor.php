0917e6212f3462623b4465888baa789cd070d103
Christopher Davis <cdavis9999@gmail.com>
2014-05-09 13:47:00 -0400

    Make the test loader work and start printing stuff

--- src/Demeanor.php
+++ src/Demeanor.php
@@ -24,6 +24,7 @@ namespace Demeanor;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command;
+use Demeanor\Exception\ConfigurationException;
 
 /**
  * The main application class.
@@ -41,15 +42,86 @@ final class Demeanor extends Command
      */
     protected function execute(InputInterface $in, OutputInterface $out)
     {
+        try {
+            $testsuites = $this->loadTestSuites();
+        } catch (\Exception $e) {
+            $out->writeln(sprintf('<error>%s</error>', $e->getMessage()));
+            return 1;
+        }
+
+        foreach ($testsuites as $testsuite) {
+            try {
+                $this->runTestSuite($testsuite, $out);
+            } catch (\Exception $e) {
+
+            }
+        }
+
         return 0;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function configure()
+    protected function configure()
     {
         $this->setName(self::NAME);
         $this->setDescription('Run all or a single test suite');
+    }
+
+    private function loadTestSuites()
+    {
+        $config = $this->loadConfiguration();
+        if (empty($config['testsuites'])) {
+            throw new ConfigurationException('No Test suites defined in configuration');
+        }
+
+        if (!is_array($config['testsuites'])) {
+            throw new ConfigurationException('`testsuites` configuration much be an object');
+        }
+
+        $factory = new TestSuiteFactory();
+        $suites = array();
+        foreach ($config['testsuites'] as $name => $suiteConfig) {
+            $suites[] = $factory->create($name, $suiteConfig);
+        }
+
+        return $suites;
+    }
+
+    private function loadConfiguration()
+    {
+        $files = ['demeanor.json', 'demeanor.dist.json'];
+        $configFile = null;
+        foreach ($files as $file) {
+            if (file_exists($file)) {
+                $configFile = $file;
+                break;
+            }
+        }
+
+        if (!$configFile) {
+            throw new ConfigurationException('No '.implode(' or ', $files). ' configuration file found');
+        }
+
+        $json = file_get_contents($configFile);
+        $config = json_decode($json, true);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            // XXX figure how to get the json error here and give a better message
+            throw new ConfigurationException('Could not load configuration json file');
+        }
+
+        return $config;
+    }
+
+    private function runTestSuite(TestSuite $suite, OutputInterface $out)
+    {
+        $suite->bootstrap();
+        $tests = $suite->load();
+
+        foreach ($tests as $test) {
+            $result = $test->run();
+            $out->writeln($test->getName() . ': ');
+        }
     }
 }
