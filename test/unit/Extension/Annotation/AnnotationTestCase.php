9230de1a03c7f20b23797e2241f30d05b38ff7f4
Christopher Davis <cdavis9999@gmail.com>
2014-05-14 10:42:20 -0400

    Add Tests for Before/After Annotations

---
+++ test/unit/Extension/Annotation/AnnotationTestCase.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright 2014 Christopher Davis <http://christopherdavis.me>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package     Demeanor
+ * @copyright   2014 Christopher Davis <http://christopherdavis.me>
+ * @license     http://opensource.org/licenses/apache-2.0 Apache-2.0
+ */
+
+namespace Demeanor\Extension\Annotation;
+
+abstract class AnnotationTestCase
+{
+    protected function testCaseMock()
+    {
+        return \Mockery::mock('Demeanor\\Unit\\UnitTestCase')
+            ->shouldReceive('getReflectionClass')
+            ->andReturn($this->reflectionClass())
+            ->getMock();
+    }
+
+    protected function testContextMock()
+    {
+        return \Mockery::mock('Demeanor\\TestContext');
+    }
+
+    protected function testResultMock()
+    {
+        return \Mockery::mock('Demeanor\\TestResult');
+    }
+
+    protected function reflectionClass()
+    {
+        return new \ReflectionClass($this);
+    }
+
+    public function cb()
+    {
+        
+    }
+
+    private function privateCb()
+    {
+        
+    }
+}
---
+++ test/unit/Extension/Annotation/BeforeTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Copyright 2014 Christopher Davis <http://christopherdavis.me>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package     Demeanor
+ * @copyright   2014 Christopher Davis <http://christopherdavis.me>
+ * @license     http://opensource.org/licenses/apache-2.0 Apache-2.0
+ */
+
+namespace Demeanor\Extension\Annotation;
+
+/**
+ * TODO whenever data providers happen this and AfterTest can be combined
+ */
+class BeforeTest extends AnnotationTestCase
+{
+    public function testNoValidCallablesDoesNotAttachBeforeCallback()
+    {
+        $testcase = $this->testCaseMock();
+        $testcase->shouldReceive('before')
+            ->never();
+        $annot = new Before([]);
+
+        $annot->attach($testcase, $this->testContextMock(), $this->testResultMock());
+    }
+
+    public function testMethodWithPrivateMethodCallbackDoesNotAddBeforeCallback()
+    {
+        $testcase = $this->testCaseMock();
+        $testcase->shouldReceive('before')
+            ->never();
+        $annot = new Before(['method' => 'privateCb']);
+
+        $annot->attach($testcase, $this->testContextMock(), $this->testResultMock());
+    }
+
+    public function testMethodThatDoesNotExistDoesNotAddBeforeCallback()
+    {
+        $testcase = $this->testCaseMock();
+        $testcase->shouldReceive('before')
+            ->never();
+        $annot = new Before(['method' => 'methodDoesNotExist']);
+
+        $annot->attach($testcase, $this->testContextMock(), $this->testResultMock());
+    }
+
+    public function testValidMethodAddsBeforeCallback()
+    {
+        $testcase = $this->testCaseMock();
+        $testcase->shouldReceive('getTestObject')
+            ->once()
+            ->andReturn($this);
+        $testcase->shouldReceive('before')
+            ->once()
+            ->with([$this, 'cb']);
+        $annot = new Before(['method' => 'cb']);
+
+        $annot->attach($testcase, $this->testContextMock(), $this->testResultMock());
+    }
+
+    public function testFunctionThatDoesNotExistDoesNotAddBeforeCallback()
+    {
+        $testcase = $this->testCaseMock();
+        $testcase->shouldReceive('before')
+            ->never();
+        $annot = new Before(['function' => 'function_does_not_exist']);
+
+        $annot->attach($testcase, $this->testContextMock(), $this->testResultMock());
+    }
+
+    public function testValidFuncionAddsBeforeCallback()
+    {
+        $testcase = $this->testCaseMock();
+        $testcase->shouldReceive('before')
+            ->once()
+            ->with('is_array');
+        $annot = new Before(['function' => 'is_array']);
+
+        $annot->attach($testcase, $this->testContextMock(), $this->testResultMock());
+    }
+}
