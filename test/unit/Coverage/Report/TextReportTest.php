5b9d7fbf8ca392760907b491363e50cb6b9ff4b1
Christopher Davis <cdavis9999@gmail.com>
2014-06-14 16:34:09 -0400

    Test the TextReport

---
+++ test/unit/Coverage/Report/TextReportTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright 2014 Christopher Davis <http://christopherdavis.me>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package     Demeanor
+ * @copyright   2014 Christopher Davis <http://christopherdavis.me>
+ * @license     http://opensource.org/licenses/apache-2.0 Apache-2.0
+ */
+
+namespace Demeanor\Coverage\Report;
+
+use Counterpart\Assert;
+
+class TextReportTest
+{
+    private $outputFile;
+    private $report;
+
+    public function __construct()
+    {
+        $this->outputFile = __DIR__.'/../../Fixtures/tmp/coverage.txt';
+        if (file_exists($this->outputFile)) {
+            unlink($this->outputFile);
+        }
+        $this->report = new TextReport($this->outputFile);
+    }
+
+    public function testRenderOutputsFileWithExpectedContents()
+    {
+        $coverage = new \ArrayObject([
+            __FILE__    => [
+                1 => 1,
+                2 => 1,
+            ],
+        ]);
+        $files = [__FILE__];
+
+        $this->report->render($coverage, $files);
+
+        Assert::assertFileExists($this->outputFile);
+        Assert::assertStringContains(__FILE__, file_get_contents($this->outputFile));
+    }
+}
