4bebdc30c2d0240f0be68e5cfdd22c99f48ed696
Christopher Davis <cdavis9999@gmail.com>
2014-06-28 19:13:25 -0400

    Call me 0.4

--- src/Demeanor.php
+++ src/Demeanor.php
@@ -43,7 +43,7 @@ use Demeanor\Exception\ConfigurationException;
  */
 final class Demeanor
 {
-    const VERSION   = '0.3';
+    const VERSION   = '0.4';
     const NAME      = 'Demeanor';
 
     const EXIT_SUCCESS      = 0;
