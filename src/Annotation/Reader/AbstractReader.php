9611232fc05ebb81a4beaf9679cbffd0e03f1fd4
Christopher Davis <cdavis9999@gmail.com>
2014-08-10 12:15:46 -0400

    Introduce DocblockReader

---
+++ src/Annotation/Reader/AbstractReader.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright 2014 Christopher Davis <http://christopherdavis.me>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package     Demeanor
+ * @copyright   2014 Christopher Davis <http://christopherdavis.me>
+ * @license     http://opensource.org/licenses/apache-2.0 Apache-2.0
+ */
+
+namespace Demeanor\Annotation\Reader;
+
+use Demeanor\TestCase;
+use Demeanor\Exception\InvalidArgumentException;
+
+/**
+ * ABC for DocblockReader implementations.
+ *
+ * @since   0.5
+ */
+abstract class AbstractReader implements DocblockReader
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function docblocksFor(TestCase $testcase)
+    {
+        if (!$this->supports($testcase)) {
+            throw new InvalidArgumentException(sprintf(
+                '"%s" is not supported by %s',
+                get_class($testcase),
+                get_class($this)
+            ));
+        }
+
+        return $this->readDocblocks($testcase);
+    }
+
+    /**
+     * Actually read the docblocks from the testcase.
+     *
+     * @since   0.5
+     * @param   TestCase $testcase
+     * @return  string[]
+     */
+    abstract protected function readDocblocks(TestCase $testcase);
+}
